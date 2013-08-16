fdb294fd40768a66fce4e78d59474af4ef933cf7
Tomas Spacek <tomspacek@gmail.com>
2013-08-16 21:22:32 +1000

    Allow manually choosing which columns to require

--- lib/active_admin_csv_import/dsl.rb
+++ lib/active_admin_csv_import/dsl.rb
@@ -1,19 +1,16 @@
 module ActiveAdminCsvImport
   module DSL
 
-    def csv_importable
+    def csv_importable(options={})
       action_item :only => :index do
         link_to "Import #{active_admin_config.resource_name.to_s.pluralize}", :action => 'import_csv'
       end
 
       collection_action :import_csv do
-        @fields = active_admin_config.resource_class.columns.map(&:name) - ["id", "updated_at", "created_at"]
+        @fields = options[:columns] ||= active_admin_config.resource_class.columns.map(&:name) - ["id", "updated_at", "created_at"]
         render "admin/csv/import_csv"
       end
 
-      collection_action :create_or_update_batch, :method => :post do
-        # Create or update from posted data.
-      end
     end
 
   end
